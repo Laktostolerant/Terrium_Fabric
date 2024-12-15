d0a08026f38b3de9b6f63804117fd5a0eddac7ef
Tiger <tigominecraft@gmail.com>
2024-12-15 20:57:10 +0100

    working on aq new test biome

--- src/main/java/com/laktostolerant/terrium/world/biome/ModBiomes.java
+++ src/main/java/com/laktostolerant/terrium/world/biome/ModBiomes.java
@@ -35,6 +35,9 @@ public class ModBiomes {
     public static final RegistryKey<Biome> DEEP_JUNGLE = RegistryKey.of(RegistryKeys.BIOME,
             Identifier.of(Terrium.MOD_ID, "deep_jungle"));
 
+    public static final RegistryKey<Biome> TEST_BIOME = RegistryKey.of(RegistryKeys.BIOME,
+            Identifier.of(Terrium.MOD_ID, "test_biome"));
+
     private static final Set<RegistryKey<Biome>> EXCLUDED_BIOMES = Set.of(
             BiomeKeys.DEEP_DARK,
             BiomeKeys.LUSH_CAVES,
@@ -46,6 +49,7 @@ public class ModBiomes {
     public static void boostrap (Registerable<Biome> context) {
         context.register(ABYSS_BIOME, abyssBiome(context));
         context.register(DEEP_JUNGLE, deepJungle(context));
+        context.register(TEST_BIOME, testBiome(context));
     }
 
     public static void globalOverworldGeneration(GenerationSettings.LookupBackedBuilder builder) {
@@ -88,6 +92,49 @@ public class ModBiomes {
     }
 
     public static Biome deepJungle(Registerable<Biome> context) {
+        SpawnSettings.Builder spawnBuilder = new SpawnSettings.Builder();
+
+        spawnBuilder.spawn(SpawnGroup.CREATURE, new SpawnSettings.SpawnEntry(EntityType.AXOLOTL, 5, 4, 4));
+
+
+        GenerationSettings.LookupBackedBuilder biomeBuilder =
+                new GenerationSettings.LookupBackedBuilder(context.getRegistryLookup(RegistryKeys.PLACED_FEATURE),
+                        context.getRegistryLookup(RegistryKeys.CONFIGURED_CARVER));
+
+
+        globalOverworldGeneration(biomeBuilder);
+        DefaultBiomeFeatures.addMossyRocks(biomeBuilder);
+        DefaultBiomeFeatures.addDefaultOres(biomeBuilder);
+        DefaultBiomeFeatures.addExtraGoldOre(biomeBuilder);
+
+        biomeBuilder.feature(GenerationStep.Feature.VEGETAL_DECORATION, VegetationPlacedFeatures.TREES_PLAINS);
+        DefaultBiomeFeatures.addForestFlowers(biomeBuilder);
+        DefaultBiomeFeatures.addLargeFerns(biomeBuilder);
+
+        DefaultBiomeFeatures.addDefaultMushrooms(biomeBuilder);
+        DefaultBiomeFeatures.addDefaultVegetation(biomeBuilder);
+
+
+
+        return new Biome.Builder()
+                .precipitation(true)
+                .downfall(0.4f)
+                .temperature(0.7f)
+                .generationSettings(biomeBuilder.build())
+                .spawnSettings(spawnBuilder.build())
+                .effects((new BiomeEffects.Builder())
+                        .waterColor(0xf68ad1)
+                        .waterFogColor(0xbf1b26)
+                        .skyColor(0x30c918)
+                        .grassColor(0x2f022f)
+                        .foliageColor(0xd203fc)
+                        .fogColor(0x22a1e6)
+                        .moodSound(BiomeMoodSound.CAVE)
+                        .music(MusicType.createIngameMusic(RegistryEntry.of(SoundEvents.BLOCK_AMETHYST_CLUSTER_HIT))).build())
+                .build();
+    }
+
+    public static Biome testBiome(Registerable<Biome> context) {
         SpawnSettings.Builder spawnBuilder = new SpawnSettings.Builder();
 
         spawnBuilder.spawn(SpawnGroup.CREATURE, new SpawnSettings.SpawnEntry(EntityType.AXOLOTL, 5, 4, 4));
